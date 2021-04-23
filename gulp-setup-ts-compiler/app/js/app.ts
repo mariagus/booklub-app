4452ec46d60ef20f9cdbac89e9c8088942e15567
Maria Gusova <masha.gu@gmail.com>
2021-04-23 14:38:50 +0100

    add gulp

--- gulp-setup-ts-compiler/app/js/app.ts
+++ gulp-setup-ts-compiler/app/js/app.ts
@@ -1,11 +1,34 @@
-interface Person {
-    name: string
-    age: number
-}
+"use strict";
 
-function displayName(person: Person) {
-    console.log(person.name)
-}
+let url: any = new URL("https://openlibrary.org/search.json");
 
-let obj = {name:'Joe', age:21}
-displayName(obj);
+const handleSubmit = () => {
+  document.querySelector("ul").innerHTML = "";
+  let authorVal: string = document.querySelector("#author").value;
+  let titleVal: string = document.querySelector("#title").value;
+
+  url.search = new URLSearchParams({
+    title: titleVal,
+    author: authorVal,
+  });
+
+  getBook();
+};
+
+let submit = document.querySelector("#submit");
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  handleSubmit();
+});
+
+const getBook = async () => {
+  let hbs = await fetch("template.hbs");
+  let hbsData = await hbs.text();
+
+  let data = await fetch(url);
+  let result = await data.json();
+
+  const template = Handlebars.compile(hbsData);
+  const html = template(result);
+  document.querySelector("ul").innerHTML = html;
+};
